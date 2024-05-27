136d6e780697f3eb3f43a3b5bbbc14f44fb637ae
Jose Mandique <jose.mandique14@gmail.com>
2024-05-26 22:09:51 -0400

    Proyecto completo

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,8 @@
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import torta from '../assets/torta.png'
 
 const HomeView = () => {
 
@@ -8,7 +12,11 @@ const HomeView = () => {
           Bienvenido a <span className="fw-bold">Happy Cake</span> 
         </h1>
         <h6> El lugar de los pasteles felices</h6>
-        🎂
+        <Row>
+        <Col>
+          <Image classname="img" src={torta}/>
+        </Col>
+      </Row>
       </Container>
     );
 }
